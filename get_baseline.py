61087ee8fc88b4f109a883edbfa9299099207503
Giacomor7 <giacomor7@gmail.com>
2025-01-26 18:26:27 +0000

    Get baseline accuracy and fairness for reweighing methods

--- get_baseline.py
+++ get_baseline.py
@@ -1,30 +1,46 @@
+import xgboost as xgb
 import numpy as np
 
-from evaluation import calculate_accuracy
+from evaluation import calculate_accuracy, demographic_parity_difference
 from model_selection import training
 
 
 def get_baseline(train_train_datasets, train_val_datasets):
+    fairness_aware = True
+
     xgboost_results = []
     rf_results = []
+    xgboost_fairness_results = []
+    rf_fairness_results = []
 
     for i in range(5):
         train_dataset_i = train_train_datasets[i]
         train_labels = train_dataset_i['label']
         train_dataset_i.drop(['label'], axis=1, inplace=True)
 
-        xgboost = training(train_dataset_i, train_labels, 'xgb')
-        rf = training(train_dataset_i, train_labels, 'rf')
+        xgboost = training(train_dataset_i, train_labels, 'xgb',
+                           fairness_aware)
+        rf = training(train_dataset_i, train_labels, 'rf', fairness_aware)
 
         val_dataset_i = train_val_datasets[i]
         labels = val_dataset_i['label']
         val_dataset_i.drop('label', axis=1, inplace=True)
 
-        xgboost_predictions = xgboost.predict(val_dataset_i)
+        dval = xgb.DMatrix(val_dataset_i, label=labels)
+
+        xgboost_predictions = xgboost.predict(dval) > 0.5
         rf_predictions = rf.predict(val_dataset_i)
 
         xgboost_results.append(calculate_accuracy(xgboost_predictions, labels))
         rf_results.append(calculate_accuracy(rf_predictions, labels))
+        xgboost_fairness_results.append(
+            demographic_parity_difference(xgboost_predictions,
+                                          val_dataset_i['DIS']))
+        rf_fairness_results.append(
+            demographic_parity_difference(rf_predictions,
+                                          val_dataset_i['DIS']))
 
     print("xgboost mean accuracy: ", np.mean(xgboost_results))
+    print("xgboost mean dpd: ", np.mean(xgboost_fairness_results))
     print("rf mean accuracy: ", np.mean(rf_results))
+    print("rf mean dpd: ", np.mean(rf_fairness_results))
--- model_selection.py
+++ model_selection.py
@@ -1,31 +1,39 @@
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 
+from fair_training import xgboost_fair_training, random_forest_fair_training
 
-def random_forest(x, y):
-    rf = RandomForestClassifier(n_estimators=100, oob_score=True,
-                                random_state=123456)
-    rf.fit(x, y)
+def random_forest(x, y, fairness_aware):
+    if fairness_aware:
+        rf = random_forest_fair_training(x, y)
+    else:
+        rf = RandomForestClassifier(n_estimators=100, oob_score=True,
+                                    random_state=123456)
+        rf.fit(x, y)
     return rf
 
 
-def xg_boost(x, y):
-    xgb = XGBClassifier(objective='binary:logistic', random_state=420)
-    xgb.fit(x, y)
-    return xgb
+def xg_boost(x, y, fairness_aware):
+    if fairness_aware:
+        xgboost = xgboost_fair_training(x, y)
+    else:
+        xgboost = XGBClassifier(objective='binary:logistic', random_state=420)
+        xgboost.fit(x, y)
+    return xgboost
 
 
-def training(x, y, model_choice):
+def training(x, y, model_choice, fairness_aware):
     """
     Train chosen model on supplied dataset
-    :param x: training examples
-    :param y: training labels
-    :param model_choice: string - 'rf' or 'xgb'
-    :return: trained model
+    :param fairness_aware: (bool) should reweighing be applied before training?
+    :param x: training examples.
+    :param y: training labels.
+    :param model_choice: string - 'rf' or 'xgb'.
+    :return: trained model.
     """
     if model_choice == 'rf':
-        return random_forest(x, y)
+        return random_forest(x, y, fairness_aware)
     elif model_choice == 'xgb':
-        return xg_boost(x, y)
+        return xg_boost(x, y, fairness_aware)
     else:
         raise ValueError("Model choice must be either 'rf' or 'xgb'")
